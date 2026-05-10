11c4ea0a46b2ff20548795feb91e6491653de3d2
trbara <trbaradaniel@icloud.com>
2026-05-10 17:03:31 +0200

    feat: UI test for registration screen

--- android/app/src/main/java/com/example/activitytracker/ui/screens/register/RegisterScreen.kt
+++ android/app/src/main/java/com/example/activitytracker/ui/screens/register/RegisterScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -178,7 +179,9 @@ fun CustomTextField(
         OutlinedTextField(
             value = value,
             onValueChange = onValueChange,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .testTag(label),
             shape = RoundedCornerShape(12.dp),
             isError = isError,
             visualTransformation = visualTransformation,
