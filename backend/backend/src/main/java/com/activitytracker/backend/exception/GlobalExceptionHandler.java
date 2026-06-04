ba8f373f55d4dbbe545eb23a5dfbb17c55315a99
mirac <mirac.i.sancak@gmail.com>
2026-06-04 18:13:36 +0200

    fix: added an Exception + added missing dependency

--- backend/backend/src/main/java/com/activitytracker/backend/exception/GlobalExceptionHandler.java
+++ backend/backend/src/main/java/com/activitytracker/backend/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -50,5 +51,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<String> handleDuplicateUser(UserAlreadyExistsException e) {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidation(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body("Validation failed");
     }
 }
