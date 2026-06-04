6780739ebf1d2ab21d75014c1a70ffc294731325
trbara <trbaradaniel@icloud.com>
2026-06-04 15:32:54 +0200

    fix: test overwritten

--- backend/backend/src/test/java/com/activitytracker/backend/BackendApplicationTests.java
+++ backend/backend/src/test/java/com/activitytracker/backend/BackendApplicationTests.java
@@ -2,17 +2,16 @@ package com.activitytracker.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
-		"spring.datasource.url=jdbc:h2:mem:testdb",
+		"spring.datasource.url=jdbc:h2:mem:testdb;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE",
 		"spring.datasource.driver-class-name=org.h2.Driver",
-		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+		"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+		"spring.jpa.hibernate.ddl-auto=create-drop"
 })
 class BackendApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
