6b8614b216725f82eb9943f43bd5f46e39df1294
Chris4733 <csakruc@gmail.com>
2026-06-06 15:30:18 +0200

    refactor: cleaned up MainActivity.kt

--- android/app/src/main/java/com/example/activitytracker/MainActivity.kt
+++ android/app/src/main/java/com/example/activitytracker/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.activitytracker
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,46 +13,24 @@ import com.example.activitytracker.ui.main.ActivityTrackerApp
 import com.example.activitytracker.widget.AddActivityWidgetIntentHandler
 import com.example.activitytracker.notification.AlarmScheduler
 import com.example.activitytracker.notification.NotificationHelper
-import android.Manifest
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.work.Constraints
-import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.ExistingWorkPolicy
-import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequestBuilder
-import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
+import com.example.activitytracker.notification.StartWeatherWorker
 import com.example.activitytracker.notification.WeatherWorker
-import java.util.concurrent.TimeUnit
+import com.example.activitytracker.premission.PermissionHandler
 
 class MainActivity : ComponentActivity() {
     private var openAddActivityRequestId by mutableStateOf(0)
 
-    private val notificationPermissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        if (!isGranted) {
-            Toast.makeText(this, "Benachrichtigungen deaktiviert", Toast.LENGTH_SHORT).show()
-        }
-        locationPermissionRequest.launch(arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION))
-    }
-    private val locationPermissionRequest = registerForActivityResult(
-        ActivityResultContracts.RequestMultiplePermissions()
-    ) { permissions ->
-        if (permissions.getOrDefault(Manifest.permission.ACCESS_COARSE_LOCATION, false)) {
-            startWeatherWorker() // Jetzt wird die Funktion gefunden
-        } else {
-            Toast.makeText(this, "Standort wird benötigt", Toast.LENGTH_SHORT).show()
-        }
+    private val PermissionHandler = PermissionHandler(this) {
+        StartWeatherWorker.scheduleWeatherUpdate(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            notificationPermissionRequest.launch(Manifest.permission.POST_NOTIFICATIONS)}
+        PermissionHandler.requestPermissions()
 
         handleIntent(intent)
 
@@ -64,33 +41,12 @@ class MainActivity : ComponentActivity() {
         }
         NotificationHelper(this).createNotificationChannel()
         AlarmScheduler(this).scheduleDailyAlarm()
-
-        if (BuildConfig.DEBUG) {
-            WorkManager.getInstance(this)
-                .enqueueUniqueWork( // WeatherWorker bei jedem App neustart triggern
-                    "TestWeather",
-                    ExistingWorkPolicy.REPLACE,
-                    OneTimeWorkRequestBuilder<WeatherWorker>().build()
-                )
-        }
     }
 
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         setIntent(intent)
         handleIntent(intent)
-    }
-
-    private fun startWeatherWorker() {
-        val workRequest = PeriodicWorkRequestBuilder<WeatherWorker>(1, TimeUnit.HOURS)
-            .setConstraints(Constraints.Builder().setRequiredNetworkType(NetworkType.CONNECTED).build())
-            .build()
-
-        WorkManager.getInstance(this).enqueueUniquePeriodicWork(
-            "WeatherWork",
-            ExistingPeriodicWorkPolicy.KEEP,
-            workRequest
-        )
     }
 
     private fun handleIntent(intent: Intent?) {
